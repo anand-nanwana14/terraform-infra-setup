d135ad99ce6db5598895aa1f022b5c149f55a899
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2024-03-29 19:50:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.rt-public_cidr_block
     gateway_id = aws_internet_gateway.gw.id
   }
 
@@ -97,7 +97,7 @@ resource "aws_route_table" "private" {
 resource "aws_route" "private" {
   count          = var.subnet_count
   route_table_id = aws_route_table.private[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
+  destination_cidr_block = var.rt-private_cidr_block
   nat_gateway_id = aws_nat_gateway.nat.id
 }
 
