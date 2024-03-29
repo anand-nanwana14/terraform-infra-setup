7bde8fb3ea2adeb82e71d807cdf5e343e6c7af2f
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2024-03-29 19:48:40 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,6 +8,16 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
+variable "rt-public_cidr_block" {
+  description = "CIDR block for public route "
+  default     = "0.0.0.0/0"
+}
+
+variable "rt-private_cidr_block" {
+  description = "CIDR block for private route "
+  default     = "0.0.0.0/0"
+}
+
 variable "vpc_name" {
   description = "Name for VPC"
   default     = "terraform-vpc"
